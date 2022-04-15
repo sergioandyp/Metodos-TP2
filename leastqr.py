67ca0a6d6f1e3df5e74df555e7a02610c194eb2f
Sergio <sergioandyp@gmail.com>
2022-04-15 15:37:38 -0300

    first commit

---
+++ leastqr.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+# Resuelve el problema de cuadrados minimos lineal utilizando descomposicion QR
+def leastq(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    pass
+
+
+def test():
+    pass
